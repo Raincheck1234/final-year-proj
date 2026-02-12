f9e2cb935ab7018eebaa771334c6d4927e8ddf3b
Tang Ziming <jp2022213090@qmul.ac.uk>
2026-02-12 09:33:18 +0800

    first commit

---
+++ src/main/java/com/raincheck/finalyearproj/FinalYearProjApplication.java
@@ -0,0 +1,13 @@
+package com.raincheck.finalyearproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalYearProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalYearProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raincheck/finalyearproj/FinalYearProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raincheck.finalyearproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalYearProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
